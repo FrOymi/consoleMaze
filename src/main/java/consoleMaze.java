512ecdba2b2c99a1a231efbd02d27954c4874fcf
FrOymi <vadimprudskyi@gmail.com>
2026-01-20 21:56:13 +0100

    Added random wall instruction for the maze

--- src/main/java/consoleMaze.java
+++ src/main/java/consoleMaze.java
@@ -9,16 +9,33 @@ public class consoleMaze {
 class Maze{
     String[][] maze;
     public void mazeGenerator(){
-        maze = new String[17][17];
-        for(int i = 0; i < maze.length; i++){
-            for(int j = 0; j < maze[i].length; j++){
-                if ((i == 0 || i == maze.length-1) || (j == 0 || j == maze[i].length-1)) {
-                    maze[i][j] = "#";
-                    continue;
+        maze = new String[15][15];
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                double randomDouble = Math.random();
+                if ((i == 0 && j == 0) || (randomDouble < 0.5)) {
+                    maze[i][j] = " ";
+                } else if (randomDouble > 0.5){
+                    if (j > 0) {
+                        maze[i][j] = maze[i][j-1].equals(" ") ? "#" : " ";
+                    } else {
+                        maze[i][j] = " ";
+                    }
+
                 }
-                maze[i][j] = " ";
             }
         }
+
+
+//        for(int i = 0; i < maze.length; i++){
+//            for(int j = 0; j < maze[i].length; j++){
+//                if ((i == 0 || i == maze.length-1) || (j == 0 || j == maze[i].length-1)) {
+//                    maze[i][j] = "#";
+//                    continue;
+//                }
+//                maze[i][j] = " ";
+//            }
+//        }
     }
     public void printMaze(){
         for (int i = 0; i < maze.length; i++) {
