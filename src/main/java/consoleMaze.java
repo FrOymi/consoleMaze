d35684b4d08e02f6947aab4022a988f4ba720e42
FrOymi <vadimprudskyi@gmail.com>
2026-01-20 20:24:03 +0100

    Added creating a playing field

--- src/main/java/consoleMaze.java
+++ src/main/java/consoleMaze.java
@@ -1,3 +1,31 @@
 public class consoleMaze {
+    public static void main(String[] args) {
+        Maze maze = new Maze();
+        maze.mazeGenerator();
+        maze.printMaze();
+    }
+}
 
+class Maze{
+    String[][] maze;
+    public void mazeGenerator(){
+        maze = new String[17][17];
+        for(int i = 0; i < maze.length; i++){
+            for(int j = 0; j < maze[i].length; j++){
+                if ((i == 0 || i == maze.length-1) || (j == 0 || j == maze[i].length-1)) {
+                    maze[i][j] = "#";
+                    continue;
+                }
+                maze[i][j] = " ";
+            }
+        }
+    }
+    public void printMaze(){
+        for (int i = 0; i < maze.length; i++) {
+            for(int j = 0; j < maze[i].length; j++){
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
