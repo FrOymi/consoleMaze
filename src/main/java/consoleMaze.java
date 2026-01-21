8a94f6b5cd92b472e2cafa1539e98bed2b849517
FrOymi <vadimprudskyi@gmail.com>
2026-01-21 22:08:50 +0100

    Added the first working version of the maze generation

--- src/main/java/consoleMaze.java
+++ src/main/java/consoleMaze.java
@@ -1,42 +1,67 @@
 public class consoleMaze {
     public static void main(String[] args) {
         Maze maze = new Maze();
-        maze.mazeGenerator();
+        maze.createMaze();
         maze.printMaze();
     }
 }
 
 class Maze{
     String[][] maze;
-    public void mazeGenerator(){
+
+    public void createMaze() {
+        mazeGenerator();
+        String[][] playingBoard = new String[maze.length][maze[0].length];
+        for (int i = 0; i < playingBoard.length; i++) {
+            for (int j = 0; j < playingBoard[0].length; j++) {
+                if (i == 0 || i == playingBoard[0].length - 1 || j == 0 || j == playingBoard[0].length - 1) {
+                    playingBoard[i][j] = "#";
+                    continue;
+                }
+                playingBoard[i][j] = maze[i - 1][j - 1];
+            }
+        }
+        playingBoard[1][0] = "S";
+        playingBoard[playingBoard.length-2][playingBoard[0].length-1] = "E";
+        maze = playingBoard;
+    }
+
+    void mazeGenerator(){
         maze = new String[15][15];
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[i].length; j++) {
                 double randomDouble = Math.random();
-                if ((i == 0 && j == 0) || (randomDouble < 0.5)) {
-                    maze[i][j] = " ";
-                } else if (randomDouble > 0.5){
-                    if (j > 0) {
-                        maze[i][j] = maze[i][j-1].equals(" ") ? "#" : " ";
-                    } else {
+                if (i == 0) {
+                    if (j == 0 || (randomDouble < 0.5)) {
                         maze[i][j] = " ";
+                    } else if (randomDouble > 0.5) {
+                        maze[i][j] = maze[i][j-1].equals(" ") ? "#" : " ";
                     }
-
+                } else if (j == 0 || j == maze[i].length - 1) {
+                    if (randomDouble < 0.5) {
+                        maze[i][j] = " ";
+                    } else if (randomDouble > 0.5) {
+                        if (j == 0) {
+                            maze[i][j] = maze[i-1][j].equals(" ") && maze[i-1][j + 1].equals(" ") ? "#" : " ";
+                        } else {
+                            maze[i][j] = maze[i-1][j].equals(" ") && i != maze[i].length - 1 ? "#" : " ";
+                        }
+                    }
+                } else {
+                    if (randomDouble < 0.5) {
+                        if (i != maze.length - 1 && maze[i - 1][j + 1].equals(" ")) {
+                            maze[i][j] = (maze[i][j-1].equals(" ") && maze[i-1][j-1].equals(" ") && maze[i-1][j].equals(" ")) ? "#" : " ";
+                        } else {
+                            maze[i][j] = " ";
+                        }
+                    }  else if (randomDouble > 0.5) {
+                        maze[i][j] = maze[i-1][j+1].equals(" ") && (maze[i][j-1].equals(" ") || maze[i-1][j-1].equals(" ") || maze[i-1][j].equals(" ")) ? "#" : " ";
+                    }
                 }
             }
         }
-
-
-//        for(int i = 0; i < maze.length; i++){
-//            for(int j = 0; j < maze[i].length; j++){
-//                if ((i == 0 || i == maze.length-1) || (j == 0 || j == maze[i].length-1)) {
-//                    maze[i][j] = "#";
-//                    continue;
-//                }
-//                maze[i][j] = " ";
-//            }
-//        }
     }
+
     public void printMaze(){
         for (int i = 0; i < maze.length; i++) {
             for(int j = 0; j < maze[i].length; j++){
@@ -45,4 +70,5 @@ class Maze{
             System.out.println();
         }
     }
+
 }
